8d31db866ffa0e3de828eda5b1d8775dccc013cd
Nyijia Morgan <mnyijia23@gmail.com>
2023-05-03 16:32:52 -0500

    homewwork

---
+++ loops.js
@@ -0,0 +1,65 @@
+//Create a loop that logs the numbers from 0-99 (ascending)
+
+for(let nums = 0; nums < 100; nums++){
+    console.log(nums);
+
+}
+
+//Create a loop that logs the numbers from 99-0 (descending)
+
+for(let number = 99; number >= 0; number--) {
+
+    console.log(number);
+
+
+}
+
+
+//Create a loop that logs the EVEN numbers from 0-98 (ascending
+
+for(let nums2 = 2; nums2 <= 98; nums2 = nums2 + 2) {
+    console.log(nums2); //couldnt get 0 here
+}
+
+
+//Create a loop that logs the EVEN numbers from 98-0 (descending)
+for(let nums2 = 98; nums2 > 0; nums2 = nums2 - 2) {
+    console.log(nums2); //couldnt get 0
+}
+    
+
+
+
+//Create a loop that logs the ODD numbers from 0-99 (ascending)
+for(let nums6 = 1; nums6 <= 99; nums6 = nums6 + 2) {
+    console.log(nums6) //couldnt get 0 here
+}
+
+//Create a loop that logs the ODD numbers from 99-0 (descending)
+for(let nums6 = 99; nums6 > 0; nums6 = nums6 - 2) {
+    console.log(nums6);  //i couldnt get it to 0
+}
+    
+
+//Create a loop that logs the numbers from 49-72 (ascending)
+for(let w = 49; w <= 72; w++) {
+    console.log(w)
+}
+
+
+
+//Create a loop that logs the numbers from 81-26 (descending)
+for(let o = 81; o >= 26; o --) {
+    console.log(o)
+
+}
+    
+
+     
+    
+
+
+//Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+//for(let nums0 = 3; nums0 < 100; nums0 = nums0 + 3) {
+    //console.log(nums0);
+//}
